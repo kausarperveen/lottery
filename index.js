7507b06c075108e229aa96ab227de3ec6ee2dc76
kausar perveen <chandamughal12@gmail.com>
2023-05-05 23:16:38 +0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 const indexRoutes = require('./routes/index');
-
-require('dotenv').config();
 
 // Import and register models
 require('./models/user');
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 app.use('/users', userRoutes);
 app.use('/', indexRoutes);
 app.set('view engine', 'ejs');
-mongoose.connect(process.env.DB_URI, {
+
+/*mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
@@ -26,5 +27,20 @@ mongoose.connect(process.env.DB_URI, {
 }).catch((error) => {
   console.log(error);
   process.exit(1);
+});*/
+// Connect to database
+mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI_PROD : process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+
+}).then(() => {
+  console.log('db connected');
+  app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
+}).catch((error) => {
+  console.log(error);
+  process.exit(1);
 });
+
+// Serve static assets in production
+
 module.exports = app;
